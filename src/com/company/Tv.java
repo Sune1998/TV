b27776f36e641ec18ae08019ff343045a31faa55
sune <sune1309@edu.easj.dk>
2019-11-19 13:43:07 +0100

    added stuff

--- src/com/company/Tv.java
+++ src/com/company/Tv.java
@@ -1,14 +1,17 @@
 package com.company;
 import java.util.Scanner;
 
+// laver en class med navnet tv, og instancere 3 var.
 public class Tv {
 int channel;
 boolean on;
 int volvl;
 
+// adds a new object named Tv.
 Tv() {
 
 }
+// adds several methods that controles the Tv.
 void turnOn() {
 on = true;
 }
