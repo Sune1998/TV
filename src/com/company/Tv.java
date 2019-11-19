6fb4be03d44c9af400080547b27c5029a3c86fff
sune <sune1309@edu.easj.dk>
2019-11-19 13:37:55 +0100

    added stuff

--- src/com/company/Tv.java
+++ src/com/company/Tv.java
@@ -10,33 +10,33 @@ Tv() {
 
 }
 void turnOn() {
-
+on = true;
 }
 void turnOff() {
-
+on = true;
 }
 
 void setChannel(int newChannel) {
-
+channel = newChannel;
 }
 
 void setVolvl(int newvolvl){
-
+volvl = newvolvl;
 }
 void channelUp() {
-
+channel++;
 }
 
 void channelDown() {
-
+channel--;
 }
 
 void volumeUp() {
-
+volvl++;
 }
 
 void volumeDown() {
-
+volvl--;
 }
 
 }
