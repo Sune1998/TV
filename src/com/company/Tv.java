fe43a4a2d1f47f8a01b04a34f6fedccf80537122
sune <sune1309@edu.easj.dk>
2019-11-19 13:31:46 +0100

    Initial commit

---
+++ src/com/company/Tv.java
@@ -0,0 +1,42 @@
+package com.company;
+import java.util.Scanner;
+
+public class Tv {
+int channel;
+boolean on;
+int volvl;
+
+Tv() {
+
+}
+void turnOn() {
+
+}
+void turnOff() {
+
+}
+
+void setChannel(int newChannel) {
+
+}
+
+void setVolvl(int newvolvl){
+
+}
+void channelUp() {
+
+}
+
+void channelDown() {
+
+}
+
+void volumeUp() {
+
+}
+
+void volumeDown() {
+
+}
+
+}
